00516ff380b576f425f67088b07d5b34bbb6568a
liuwentao <liuwentao@gmail.com>
2021-05-11 06:18:26 -0400

    fix:uid不生效

--- tiddly/entrypoint.sh
+++ tiddly/entrypoint.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env sh
-PUID=${PUID:-911}
-PGID=${PGID:-911}
-groupmod -o -g "$PGID" abc
-usermod -o -u "$PUID" abc
-echo "
-User uid:    $(id -u abc)
-User gid:    $(id -g abc)
--------------------------------------
-"
-
-groupmod -o -g "$PGID" abc
-usermod -o -u "$PUID" abc
-
-chown abc:abc /app
 
 if [ ! -f "/app/tiddlywiki.info" ]; then
     tiddlywiki /app --init server
